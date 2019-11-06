af3fdecdd47d91e38941929814aa5905d9173a05
FSDonnelly <donnelly_frank@yahoo.com>
2019-11-05 21:46:54 -0800

    connected mongoDB and added posts.js model

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 let express = require('express');
 let app = express();
+let mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/travels', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 let PORT = process.env.PORT || 3000;
+
+let Post = require('./models/posts').Post;
+
+let post1 = new Post({
+  id: 2,
+  title: `Statue of Liberty`,
+  date: new Date(),
+  description: `Some description`,
+  text: `Some Text`,
+  country: `USA`,
+  imageURL: `/images/2.jpg`
+});
+
+post1.save();
 
 app.use(express.static('public'));
 app.use(express.json());
