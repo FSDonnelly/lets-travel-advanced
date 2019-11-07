3bdc0225ce423fa138a99b0c1f9487429769e901
FSDonnelly <donnelly_frank@yahoo.com>
2019-11-06 23:19:09 -0800

    create post form and logic

--- app.js
+++ app.js
@@ -7,17 +7,33 @@ mongoose.connect('mongodb://localhost/travels', {
   useUnifiedTopology: true
 });
 
+app.use(express.static('public'));
+app.use(express.json());
+
 let PORT = process.env.PORT || 3000;
 
 let Post = require('./models/posts').Post;
+let id = 1;
 
 app.get(`/posts`, async (req, res) => {
   let posts = await Post.find();
   res.send(posts);
 });
 
-app.use(express.static('public'));
-app.use(express.json());
+app.post(`/posts`, async (req, res) => {
+  let reqBody = req.body;
+  let newPost = new Post({
+    id: id++,
+    title: reqBody.title,
+    date: new Date(),
+    description: reqBody.description,
+    text: reqBody.text,
+    country: reqBody.country,
+    imageURL: reqBody.imageUrl
+  });
+  await newPost.save();
+  res.send('Created');
+});
 
 app.listen(PORT, () =>
   console.log(`Server started on http://localhost:${PORT}`)
