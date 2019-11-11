7e9f7e33249467632663ad408ad8d83cccf1019e
FSDonnelly <donnelly_frank@yahoo.com>
2019-11-10 23:36:47 -0800

    add email delete to admin page

--- public/admin/js/emails.js
+++ public/admin/js/emails.js
@@ -3,3 +3,16 @@ getEmails = async () => {
     .then(res => res.json())
     .then(data => data);
 };
+
+let emailsBlock = document.querySelector('#v-pills-mails');
+
+emailsBlock.addEventListener('click', function(e) {
+  if (e.target.classList.contains('btn-remove')) {
+    let id = e.target.parentNode.parentNode.querySelector('.id').value;
+    fetch(`http://localhost:3000/emails/${id}`, {
+      method: 'DELETE'
+    })
+      .then(res => res.text())
+      .then(() => window.history.go());
+  }
+});
