0eeb14a21f1bd4ae43d9d2ef202506154ce63530
FSDonnelly <donnelly_frank@yahoo.com>
2019-11-10 22:52:46 -0800

    add email model and route to back end

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let mongoose = require('mongoose');
 let multer = require('multer');
 let postsRouter = require('./routes/posts');
 let callbacksRouter = require('./routes/callback-requests');
+let emailsRouter = require('./routes/emails');
 
 mongoose.connect('mongodb://localhost/travels', {
   useNewUrlParser: true,
@@ -23,6 +24,7 @@ let PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use('/posts', postsRouter);
 app.use('/callbacks', callbacksRouter);
+app.use('/emails', emailsRouter);
 
 app.listen(PORT, () =>
   console.log(`Server started on http://localhost:${PORT}`)
---
+++ routes/emails.js
@@ -0,0 +1,31 @@
+let uuid = require('uuid');
+let express = require('express');
+let router = express.Router();
+
+let Email = require('../models/emails').Email;
+
+router.get('/', async (req, res) => {
+  let emails = await Email.find();
+  res.send(emails);
+});
+
+router.post('/', async (req, res) => {
+  let reqBody = req.body;
+  let newEmail = new Email({
+    id: uuid(),
+    email: reqBody.email,
+    name: reqBody.name,
+    text: reqBody.text,
+    date: new Date()
+  });
+  await newEmail.save();
+  res.send('Email Accepted');
+});
+
+router.delete('/:id', async (req, res) => {
+  let id = req.params.id;
+  await Email.deleteOne({ id });
+  res.send('Email Deleted!');
+});
+
+module.exports = router;
