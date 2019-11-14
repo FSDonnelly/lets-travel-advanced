80dc6f3547b1f3771d4082fc7a2aeaf73ed5738b
FSDonnelly <donnelly_frank@yahoo.com>
2019-11-13 23:12:21 -0800

    add zoom and drag function to map

--- public/js/maps.js
+++ public/js/maps.js
@@ -4,11 +4,35 @@ let platform = new H.service.Platform({
   app_code: "mA4-UGFZXlb2ZtlnpDic-A"
 });
 
-// Obtain the default map types from the platform object:
-let defaultLayers = platform.createDefaultLayers();
+function landmarkGeocode() {
+  let title = document.querySelector("h1").textContent;
+  let geocoder = platform.getGeocodingService(),
+    landmarkGeocodingParameters = {
+      searchtext: title,
+      jsonattributes: 1
+    };
 
-// Instantiate (and display) a map object:
-let map = new H.Map(document.querySelector(".map"), defaultLayers.normal.map, {
-  zoom: 10,
-  center: { lat: 51.500876, lng: -0.124647 }
-});
+  geocoder.search(landmarkGeocodingParameters, showMap, err =>
+    console.log(err)
+  );
+}
+
+function showMap(result) {
+  let location =
+    result.response.view[0].result[0].place.locations[0].displayPosition;
+
+  let defaultLayers = platform.createDefaultLayers();
+
+  let map = new H.Map(
+    document.querySelector(".map"),
+    defaultLayers.normal.map,
+    {
+      zoom: 15,
+      center: { lat: location.latitude, lng: location.longitude }
+    }
+  );
+
+  let ui = H.ui.UI.createDefault(map, defaultLayers);
+}
+
+landmarkGeocode();
